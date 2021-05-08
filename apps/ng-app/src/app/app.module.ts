7266eb4586d8bce1ab7ace9a18f0d760d3be864c
Cesar Sabater <cesar.sabater@tabmo.io>
2021-05-08 18:58:59 +0200

    create core module + add ngrx to app

--- apps/ng-app/src/app/app.module.ts
+++ apps/ng-app/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent],
